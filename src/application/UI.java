3ba3556aac090c09c4b42799bcdc535bd050217e
wjdsantos <walter4asantos@gmail.com>
2020-11-24 11:45:08 -0300

    Chess layer and printing the board

---
+++ src/application/UI.java
@@ -0,0 +1,27 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {
+
+	public static void printBoard(ChessPiece[][] pieces) {
+		for (int i=0; i<pieces.length; i++) {  //Imprimindo o tabuleiro
+			System.out.print((8 - i) + " ");
+			for (int j=0; j<pieces.length; j++) {
+				printPiece(pieces[i][j]);
+			}
+			System.out.println();  //Quebra de linha
+		}
+		System.out.println("  a b c d e f g h");
+	}
+	
+	private static void printPiece(ChessPiece piece) {
+		if (piece == null) {  //Imprimindo uma peça
+			System.out.print("-");
+		}
+		else {
+			System.out.println(piece);
+		}
+		System.out.print(" ");
+	}
+}
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,18 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {  //setColor foi tirado para que a cor da peça não seja alterada
+		return color;
+	}
+}
