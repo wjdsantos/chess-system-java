7475d4029b07b0aefad38b6103b7a7e9be5b5a66
wjdsantos <walter4asantos@gmail.com>
2020-11-30 17:26:50 -0300

    Bishop

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -5,15 +5,15 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Bishop extends ChessPiece {
 
-	public Rook(Board board, Color color) {
+	public Bishop(Board board, Color color) {
 		super(board, color);
 	}
 
 	@Override
 	public String toString() {
-		return "R";
+		return "B";
 	}
 	
 	@Override
@@ -21,41 +21,41 @@ public class Rook extends ChessPiece {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0, 0);
 		
-		//above - Verificando as posições acima da peça
-		p.setValues(position.getRow() - 1, position.getColumn());
+		//NorthWest NW - Verificando as posições a NW da peça
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() - 1);
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);;
 		}
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {  //Se houver mais uma posição acima e for uma peça oponente
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {  //Se houver mais uma posição em NW e for uma peça oponente
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		//left - Verificando as posições a esquerda da peça
-		p.setValues(position.getRow(), position.getColumn() - 1);
+		//NorthEast - Verificando as posições a NE da peça
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {  //Se houver mais uma posição a esquerda e for uma peça oponente
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		//right - Verificando as posições a direita da peça
-		p.setValues(position.getRow(), position.getColumn() + 1);
+		//SouthWest SW - Verificando as posições a SW da peça
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() + 1);
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {  //Se houver mais uma posição a direita e for uma peça oponente
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		//below - Verificando as posições abaixo da peça
-		p.setValues(position.getRow() + 1, position.getColumn());
+		//SouthEast - Verificando as posições SE da peça
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() + 1);
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {  //Se houver mais uma posição abaixo e for uma peça oponente
 			mat[p.getRow()][p.getColumn()] = true;
